1070bed92fa5a7919bc0e417e3ef79de3c73f026
Ravi Shanker <ravi.program@gmail.com>
2014-01-05 22:28:40 +1100

    Added answerkey model

---
+++ FlashCards/FCAnswerKey.h
@@ -0,0 +1,16 @@
+//
+//  FCAnswerKey.h
+//  FlashCards
+//
+//  Created by Ravi on 5/01/2014.
+//  Copyright (c) 2014 ikite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FCAnswerKey : NSObject
+
+- (NSMutableArray *) allocStateCards;
+- (NSMutableArray *) allocCapitalCards;
+
+@end
